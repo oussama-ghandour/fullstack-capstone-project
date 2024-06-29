f1e5bf0f019bc081e375bf35611949b26006b99d
oussama-ghandour <oussam92.ing@gmail.com>
2024-06-29 13:16:19 -0400

    update auth

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -54,4 +54,43 @@ router.post('/register', async (req, res) => {
          return res.status(500).send('Internal server error');
     }
 });
+
+// login endpoint
+router.post('/login', async (req, res) => {
+    try {
+        // Connect to `giftsdb`
+        const db  = await connectToDatabase();
+        // Access MongoDB `users` collection
+        collection = db.collection('users');
+        // Check for user credentials in db
+        theUser = await collection.findOne({ email: req.body.email});
+        // Check if the password matches 
+        if (theUser) {
+            let result = await bcryptjs.compare(req.body.password, theUser.password)
+            if (!result) {
+                logger.error('Password do not match');
+                return res.status(404).json({ error: 'Wrong password'});
+            }
+
+             let payload = {
+                  user: {
+                    id: theUser_id.toString(),
+                },
+            };
+            const userName =  theUser.firstName;
+            const userEmail = theUser.email;
+            const authtoken = jwt.sign(payload, JWT_SECRET);
+            logger.info('User logged successfully');
+            return  res.status(200).json({authtoken, userName, userEmail });  
+        } else {
+            logger.error('User not found');
+            return res.status(404).json({error: 'User not found'});
+
+        }
+    } catch (e) {
+        logger.error(e);
+         return res.status(500).send('Internal server error');
+
+    }
+});
 module.exports = router;
